f21936ba9b520a66e29de221fe37c1dc83cba2ee
Paynet <53261329+paynett@users.noreply.github.com>
2021-04-21 21:36:15 +0300

    Delete admin directory

--- admin/language/english/payment/paynet.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Heading
-$_['heading_title']      = 'Paynet';
-
-// Text
-$_['text_payment']       = 'Ödeme Metodları';
-$_['text_success']       = 'Başarılı: Ayarlarınız güncellendi!';
-$_['text_edit']          = 'Paynet Entegrasyon Bilgileriniz';
--- admin/language/turkish/payment/paynet.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Heading
-$_['heading_title']      = 'Paynet';
-
-// Text
-$_['text_payment']       = 'Ödeme Metodları';
-$_['text_success']       = 'Başarılı: Banka havalesi detayları güncellendi!';
-$_['text_edit']          = 'iPara Entegrasyon Bilgileriniz';
